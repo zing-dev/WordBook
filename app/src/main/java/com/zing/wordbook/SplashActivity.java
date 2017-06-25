249f72b9b67da38bac6d80f658b3948c777f5765
zhangrxiang <zhangrxiang@yahoo.com>
2017-06-25 18:43:41 +0800

    启动页禁止返回，图片添加圆角类

--- app/src/main/java/com/zing/wordbook/SplashActivity.java
+++ app/src/main/java/com/zing/wordbook/SplashActivity.java
@@ -4,6 +4,12 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.TextView;
+
+import com.zing.wordbook.view.RoundRectImageView;
+
+import java.util.Random;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,7 +17,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        String[] strings = {
+                "Great minds have purpose, others have wishes.",
+                "Being single is better than being in an unfaithful relationship.",
+                "If you find a path with no obstacles, it probably doesn’t lead anywhere.",
+                "Getting out of bed in winter is one of life’s hardest mission.",
+                "The future is scary but you can’t just run to the past cause it’s familiar.",
+                "I love it when I catch you looking at me then you smile and look away." ,
+                "Having a calm smile to face with being disdained indicates kind of confidence.",
+                "Success is the ability to go from one failure to another with no loss of enthusiasm."
+        };
+        TextView tv_str = (TextView) findViewById(R.id.tv_str);
 
+        RoundRectImageView rectImageView = (RoundRectImageView) findViewById(R.id.imageView);
+        rectImageView.setRoundPx(1000);
+
+        tv_str.setText(strings[new Random().nextInt(strings.length)]);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -21,15 +42,15 @@ public class SplashActivity extends AppCompatActivity {
                 finish();
             }
         }, 3000);
-//        try {
-//            Thread.sleep(30000);
-//            Intent intent = new Intent(this,MainActivity.class);
-//            intent.setFlags(1);
-//            startActivity(intent);
-//            finish();
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
+    }
 
+    /**
+     * 禁用返回
+     * @param keyCode  keyCode
+     * @param event event
+     * @return boolean
+     */
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return !(keyCode == KeyEvent.KEYCODE_BACK);
     }
 }
